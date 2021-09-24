d2102e082f3a8c707ca743101695d3f9b8cd2bde
Piotr Maślanka <pmaslanka@smok.co>
2021-09-24 15:00:00 +0200

    adapt to Python 3

--- commence_primary_ignition.py
+++ commence_primary_ignition.py
@@ -1,34 +1,33 @@
-#!/usr/bin/python
-from __future__ import print_function
-
 import random
 import os
 import string
 import threading
 import requests
+from satella.coding.concurrent import TerminableThread, ThreadCollection
+from satella.os import hang_until_sig
 
 
 def rand_string(n):
-    return b''.join(random.choice(string.ascii_lowercase) for _ in range(n))
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(n))
 
 
-class CounterTerroristsWin(threading.Thread):
-    def run(self):
-        while True:
-            email = rand_string(random.randint(5, 15)) + '@' + rand_string(random.randint(5, 15)) + '.' + rand_string(2)
-            password = rand_string(random.randint(10, 30))
+class CounterTerroristsWin(TerminableThread):
+    def loop(self):
+        email = rand_string(random.randint(5, 15)) + '@' + rand_string(random.randint(5, 15)) + '.' + rand_string(2)
+        password = rand_string(random.randint(10, 30))
 
-            r = requests.post('http://www.pmodavao.com/regs/js/dt/crypt/pass.php', data={'email': email})
+        r = requests.post('http://www.pmodavao.com/regs/js/dt/crypt/pass.php', data={'email': email})
 
-            if r.status_code == 200:
-                r = requests.post('http://www.pmodavao.com/regs/js/dt/crypt/post.php',
-                                  data={'email': email, 'password': password})
+        if r.status_code == 200:
+            r = requests.post('http://www.pmodavao.com/regs/js/dt/crypt/post.php',
+                              data={'email': email, 'password': password})
 
-                if r.status_code != 200:
-                    print('And when Im gone just carry on')
+            if r.status_code != 200:
+                print('And when Im gone just carry on')
 
 
 if __name__ == '__main__':
-    q = [CounterTerroristsWin() for _ in xrange(20)]
-    for q in q:
-        q.start()
+    q = ThreadCollection.from_class(CounterTerroristsWin, range(20))
+    q.start()
+    hang_until_sig()
+    q.terminate().join()
